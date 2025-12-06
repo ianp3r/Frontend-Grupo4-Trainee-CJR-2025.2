0972543d68d45abd5dbc261cb13a00d3f7155e2e
Gabriel Chaves <gab.gtsc@gmail.com>
2025-12-06 01:50:22 -0300

    feat/torna header funcional, faz página de feed utilizar componente unificado

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -6,8 +6,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import mascote from '@/assets/mascote.png'
 import logo from '@/assets/logo.svg'
-import { useState, FormEvent } from 'react'
-import { useRouter } from 'next/navigation' // Para redirecionar
+import { FormEvent } from 'react'
 
 const TelaDeCadastro = () => {
     const router = useRouter()

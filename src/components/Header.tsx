69bed9c5d849b25aec35eda9c07ee5a5368e06b6
Gabriel Chaves <gab.gtsc@gmail.com>
2025-12-06 01:02:15 -0300

    adiciona links aos icones do header, torna o botao de lockout funcional. Remove botoes sem pagina correspondente

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,35 @@
+'use client'
+
 import Image from 'next/image';
+import Link from 'next/link';
+import { useAuth } from '@/contexts/AuthContext';
 import logo_small from '@/assets/logo_small.svg'
-import bag from '@/assets/bag.svg';
 import exit from '@/assets/exit.svg';
 // import person from '@/assets/person.svg';
 import person_active from '@/assets/person_active.svg';
-import store from '@/assets/store.svg';
 
 const Header = () => {
+    const { logout } = useAuth();
+
     return (
         <header className='w-full h-[92px] bg-black flex items-center'>
             <div className='w-full flex justify-between mx-[72.88px]'>
-                <div>
+                <Link href='/feed-logado'>
                     <Image 
                         src={logo_small} alt='logo'
-                        className='w-[220.45px] h-[42.5px]'
+                        className='w-[220.45px] h-[42.5px] cursor-pointer'
                     />
-                </div>
+                </Link>
                 <ul className='flex gap-[30px]'>
                     <li className='w-[36px] h-[36px]'>
-                        <Image src={bag} alt='bag'/>
+                        <Link href='/perfil'>
+                            <Image src={person_active} alt='perfil' className='cursor-pointer'/>
+                        </Link>
                     </li>
                     <li className='w-[36px] h-[36px]'>
-                        <Image src={store} alt='bag'/>
-                    </li>
-                    <li className='w-[36px] h-[36px]'>
-                        <Image src={person_active} alt='bag'/>                        
-                    </li>
-                    <li className='w-[36px] h-[36px]'>
-                        <Image src={exit} alt='bag'/>                        
+                        <button onClick={logout} className='cursor-pointer'>
+                            <Image src={exit} alt='sair'/>
+                        </button>
                     </li>
                 </ul>
             </div>
